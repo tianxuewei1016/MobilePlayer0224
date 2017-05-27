9032348a55d1d76b73738f5b9144af750bf1f4cb
tianxuewei1016 <93226539@qq.com>
2017-05-27 09:48:44 +0800

    解决歌词乱码问题

--- app/src/main/java/com/atguigu/mobileplayer0224/utils/LyricUtils.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/utils/LyricUtils.java
@@ -2,6 +2,7 @@ package com.atguigu.mobileplayer0224.utils;
 
 import com.atguigu.mobileplayer0224.bean.Lyric;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,9 +26,10 @@ public class LyricUtils {
 
     /**
      * 歌词是否存在
+     *
      * @return
      */
-    public boolean isLyric(){
+    public boolean isLyric() {
         return isLyric;
     }
 
@@ -52,7 +54,7 @@ public class LyricUtils {
             try {
                 //文件输入流
                 fis = new FileInputStream(file);
-                InputStreamReader streamReader = new InputStreamReader(fis, "GBK");
+                InputStreamReader streamReader = new InputStreamReader(fis, getCharset(file));
                 BufferedReader reader = new BufferedReader(streamReader);
                 String line;
                 while ((line = reader.readLine()) != null) {
@@ -87,6 +89,72 @@ public class LyricUtils {
                 }
             }
         }
+    }
+
+    /**
+     * 判断文件编码
+     *
+     * @param file 文件
+     * @return 编码：GBK,UTF-8,UTF-16LE
+     */
+    public String getCharset(File file) {
+        String charset = "GBK";
+        byte[] first3Bytes = new byte[3];
+        try {
+            boolean checked = false;
+            BufferedInputStream bis = new BufferedInputStream(
+                    new FileInputStream(file));
+            bis.mark(0);
+            int read = bis.read(first3Bytes, 0, 3);
+            if (read == -1)
+                return charset;
+            if (first3Bytes[0] == (byte) 0xFF && first3Bytes[1] == (byte) 0xFE) {
+                charset = "UTF-16LE";
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xFE
+                    && first3Bytes[1] == (byte) 0xFF) {
+                charset = "UTF-16BE";
+                checked = true;
+            } else if (first3Bytes[0] == (byte) 0xEF
+                    && first3Bytes[1] == (byte) 0xBB
+                    && first3Bytes[2] == (byte) 0xBF) {
+                charset = "UTF-8";
+                checked = true;
+            }
+            bis.reset();
+            if (!checked) {
+                int loc = 0;
+                while ((read = bis.read()) != -1) {
+                    loc++;
+                    if (read >= 0xF0)
+                        break;
+                    if (0x80 <= read && read <= 0xBF)
+                        break;
+                    if (0xC0 <= read && read <= 0xDF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF)
+                            continue;
+                        else
+                            break;
+                    } else if (0xE0 <= read && read <= 0xEF) {
+                        read = bis.read();
+                        if (0x80 <= read && read <= 0xBF) {
+                            read = bis.read();
+                            if (0x80 <= read && read <= 0xBF) {
+                                charset = "UTF-8";
+                                break;
+                            } else
+                                break;
+                        } else
+                            break;
+                    }
+                }
+            }
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return charset;
     }
 
     /**
