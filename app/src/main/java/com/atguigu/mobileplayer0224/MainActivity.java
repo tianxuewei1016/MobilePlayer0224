0cb80277a2ae68e1075ffa3277297d1ab30d24a7
tianxuewei1016 <93226539@qq.com>
2017-05-27 10:33:35 +0800

    增加软件退出功能

--- app/src/main/java/com/atguigu/mobileplayer0224/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplayer0224/MainActivity.java
@@ -5,11 +5,14 @@ import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.atguigu.mobileplayer0224.base.BaseFragment;
 import com.atguigu.mobileplayer0224.fragment.LocalAudioFragment;
@@ -138,5 +141,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         Log.e("TAG", "onDestroy");
+    }
+
+    /**
+     * 是否已经退出--false
+     */
+    private boolean isExit = false;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (position != 0) {
+                rg_main.check(R.id.rb_local_video);
+                return true;
+            } else if (!isExit) {
+                Toast.makeText(MainActivity.this, "再按一次退出软件", Toast.LENGTH_SHORT).show();
+                isExit = true;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        isExit = false;
+                    }
+                }, 2000);
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
